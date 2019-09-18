19d178ec040ac4e151ce6fc54037db9131425eb3
crizz <c.negrea@hsag.info>
2019-09-18 13:35:58 +0200

    [TASK] Compatibility Typo3 8 & Typo3 9

--- bin/sync_master_files.sh
+++ bin/sync_master_files.sh
@@ -25,6 +25,24 @@ syncmasterfiles () {
         exit
     fi
 
+    if [ -d "web/fileadmin" ]
+    then
+        rsync -avz --exclude '**/_processed_' ${master_user}@${master_url}:${master_path}/fileadmin/* web/fileadmin
+    else
+        echo "WARING!! - No Local Fileadmin folder found in web/fileadmin"
+        echo
+        exit
+    fi
+
+    if [ -d "web/uploads" ]
+    then
+        rsync -avz --exclude ${master_user}@${master_url}:${master_path}/uploads/* web/uploads
+    else
+        echo "WARING!! -  No Local Uploads folder found in web/uploads"
+        echo
+        exit
+    fi
+
     echo
     echo
 }
